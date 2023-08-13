1ec28f07d7a2379a9452c58aa758aa2264cafa87
Vet <you@example.com>
2023-08-13 13:47:14 +0200

    task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let result = '';
+    const strN = n.toString();
+    const lN = strN.length - 1;
+    for (let i = 0; i < strN.length; i++){
+        result = strN[i] + result;
+    }
+    if (n < 0){
+        result = result.slice(0,-1);
+    }
+    return +result
 }
